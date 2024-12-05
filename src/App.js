a85740447064959260c92fabbbc2c3e8adf371a4
hp <zekariastamiru12@gmail.com>
2024-12-05 12:32:47 +0300

    adding responsive stylesheet

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import "./index.css";
 
 export default function App() {
   const [amount, setAmount] = useState(1);
@@ -6,7 +7,7 @@ export default function App() {
   const [toCur, setToCur] = useState("EUR");
   const [converted, setConverted] = useState();
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
+  const [error, setError] = useState(null);
 
   useEffect(
     function () {
@@ -38,18 +39,20 @@ export default function App() {
     },
     [amount, fromCur, toCur]
   );
+
   return (
-    <div>
+    <div className="app-container">
       <input
         type="text"
+        className="input-amount"
         value={amount}
         onChange={(e) => setAmount(Number(e.target.value))}
         disabled={isLoading}
       />
       <select
+        className="currency-selector from-currency"
         value={fromCur}
         onChange={(e) => setFromCur(e.target.value)}
-        disabled={isLoading}
       >
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
@@ -57,6 +60,7 @@ export default function App() {
         <option value="INR">INR</option>
       </select>
       <select
+        className="currency-selector to-currency"
         value={toCur}
         onChange={(e) => setToCur(e.target.value)}
         disabled={isLoading}
@@ -66,7 +70,14 @@ export default function App() {
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <p>{isLoading ? <p>Loading...</p> : `${converted} ${toCur}`}</p>
+      <p className="conversion-result">
+        {isLoading ? (
+          <p className="loading-text">Loading...</p>
+        ) : (
+          `${converted} ${toCur}`
+        )}
+      </p>
+      {error && <p className="error-message">{error}</p>}
     </div>
   );
 }
